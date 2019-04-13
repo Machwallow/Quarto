fccc34382b8c506d8fe8a74889a14556c4376c68
Stan Monnier <stanislasmonnier@hotmail.fr>
2019-04-13 18:26:55 +0200

    Interfaces accueil + langage

---
+++ src/src/controller/AccueilController.java
@@ -0,0 +1,88 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class AccueilController {
+
+    private static ResourceBundle bundle;
+    public Button buttonRules;
+    public Button buttonQuit;
+    public AnchorPane mainPane;
+    public Button buttonLocal;
+    public Button buttonLanguage;
+
+    @FXML
+    private void initialize() {
+        setupBundle();
+        setupButtonLanguage();
+        setupButtonQuit();
+    }
+
+    private void setupButtonLocal(){
+        /*buttonLocal.setOnAction(event ->{
+            try {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../vue/partieLocal.fxml"), Services.getBundle());
+                mainPane.getChildren().setAll(pane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });*/
+    }
+
+    private void setupButtonRules() {
+       /* buttonRules.setOnAction(event -> {
+            try {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../vue/rules.fxml"), Services.getBundle());
+                mainPane.getChildren().setAll(pane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });*/
+    }
+
+    private void setupButtonLanguage(){
+        buttonLanguage.setOnAction(event ->{
+            try {
+
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/language.fxml"), bundle);
+                mainPane.getChildren().setAll(pane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setupButtonQuit() {
+        buttonQuit.setOnAction(event -> {
+            try {
+                ((Stage) buttonQuit.getScene().getWindow()).close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void setupBundle(){
+        Locale.setDefault(Locale.FRANCE);
+        bundle = ResourceBundle.getBundle("bundles.bundle", Locale.getDefault());
+    }
+
+    public static void changeBundle(Locale l){
+        Locale.setDefault(l);
+        bundle = ResourceBundle.getBundle("bundles.bundle", l);
+    }
+
+    public static ResourceBundle getBundle(){
+        return bundle;
+    }
+
+}
---
+++ src/src/controller/LanguageControlleur.java
@@ -0,0 +1,63 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.File;
+import java.util.Locale;
+
+public class LanguageControlleur {
+
+    public ImageView ivFR, ivUS;
+    public Button buttonBack;
+    public AnchorPane mainPane;
+
+    @FXML
+    public void initialize(){
+       // setupivFR();
+        //setupivUS();
+        setupButtonBack();
+    }
+
+    private void setupivFR(){
+        ivFR.setOnMouseClicked(event -> {
+            AccueilController.changeBundle(Locale.FRANCE);
+            System.out.println("language changed to fr_FR");
+            try {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/accueil.fxml"));
+                mainPane.getChildren().setAll(pane);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setupivUS(){
+        ivUS.setOnMouseClicked(event -> {
+            AccueilController.changeBundle(Locale.FRANCE);
+            System.out.println("language changed to en_US");
+            try {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/accueil.fxml"));
+                mainPane.getChildren().setAll(pane);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setupButtonBack() {
+        buttonBack.setOnAction(event -> {
+            try {
+                System.out.println((new File(".").getAbsolutePath()));
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/accueil.fxml"));
+                mainPane.getChildren().setAll(pane);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+        //Services.setBackToAccueil(buttonBack, mainPane);
+    }
+}
